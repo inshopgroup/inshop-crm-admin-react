1cc3a382d032141e7b937843f6c1dd30bd9d9379
Valerii Mykhailovskyi <v.mykhailovskyi@inshop.com.ua>
2022-12-16 17:20:26 +0200

    add product

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -5,11 +5,17 @@ import {DataGrid} from '@mui/x-data-grid';
 import axios from "axios";
 import {useEffect, useState} from "react";
 import {Typography} from "@mui/material";
+import ModalWindow from '../src/components/ModalWindow'
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
 
 function Dashboard() {
     const [products, setProducts] = useState<[]>([])
     const [loading, setLoading] = useState(false)
+    const [product, setProduct] = useState({})
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(!open);
 
     const columns = [
         { field: 'id', headerName: 'id', width: 100},
@@ -25,6 +31,22 @@ function Dashboard() {
         setLoading(false)
 
     }
+    function getEmptyObj() {
+        return {
+            id: Date.now(),
+            name: '',
+            prop2: '',
+            prop3: ''
+        };
+    }
+    function change(prop, event) { // изменение свойства при вводе
+        setProduct({...product, id: Date.now(), [prop]: event.target.value});
+    }
+    function add() {
+        setProducts([...products, product]);  // добавление объекта к массиву
+        setProduct(getEmptyObj()); // сохранение пустого объекта в стейт
+        setOpen(!open)
+    }
     useEffect(() => {
         fetchProducts()
 
@@ -32,6 +54,11 @@ function Dashboard() {
     return(
         <MainLayout>
             <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                    <Button onClick={handleOpen}>Add Product</Button>
+                <ModalWindow open={open} handleOpen={handleOpen} item={product} change={change} add={add} item={
+                }
+                >
+                </ModalWindow>
                 <div style={{ height: 600, width: '100%' }}>
                     { loading && <Typography variant="h6" align="center">Loading...</Typography> }
                     <DataGrid rows={products} columns={columns} checkboxSelection />
---
+++ src/components/ModalWindow.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import {useState} from "react";
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+export default function ModalWindow(props: any) {
+
+    return (
+        <div>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={props.open}
+                onClose={props.handleOpen}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={props.open}>
+                    <Box sx={style}>
+                        <button onClick={props.add}>Добавить объект</button>
+                    </Box>
+                </Fade>
+            </Modal>
+        </div>
+    );
+}
