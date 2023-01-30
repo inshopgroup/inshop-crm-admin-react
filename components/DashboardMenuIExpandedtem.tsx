72f8f50f1103f3885b2bd26aded1696fa210173d
Yurii Salashnyi <yurik.salashnuy@gmail.com>
2023-01-30 18:41:11 +0100

    Dashboard menu

--- components/DashboardMenuIExpandedtem.tsx
+++ components/DashboardMenuIExpandedtem.tsx
@@ -1,10 +1,5 @@
 import * as React from 'react'
 import List from '@mui/material/List'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
-import ExpandLess from '@mui/icons-material/ExpandLess'
-import ExpandMore from '@mui/icons-material/ExpandMore'
 import DashboardMenuItem from './DashboardMenuItem'
 import Collapse from '@mui/material/Collapse'
 
@@ -17,24 +12,16 @@ interface DashboardMenuExpandedItemProps {
 export default function DashboardMenuExpandedItem(props: DashboardMenuExpandedItemProps) {
   const { item } = props
   const [open, setOpen] = React.useState<boolean>(false)
-  const handleClick = (): void => {
-    setOpen(!open)
-  };
   const hasChildren = item.children && item?.children?.length > 0
 
   return hasChildren ? (
       <>
-        <ListItemButton onClick={handleClick}>
-          <ListItemIcon>
-            <props.item.icon />
-          </ListItemIcon>
-          <ListItemText primary={item.name} />
-          {open ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
+        <DashboardMenuItem item={item} hasChildren={hasChildren} open={open} onClick={setOpen} />
+
         <Collapse in={open} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
-            {(item.children || []).map((item: IMenuItem, index) => (
-                <DashboardMenuItem item={item} pl={4} key={index} />
+            {(item.children || []).map((child: IMenuItem, index) => (
+                <DashboardMenuItem item={child} pl={4} key={index} />
             ))}
           </List>
         </Collapse>
--- components/DashboardMenuItem.tsx
+++ components/DashboardMenuItem.tsx
@@ -2,23 +2,37 @@ import * as React from 'react'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
+import ExpandLess from '@mui/icons-material/ExpandLess'
+import ExpandMore from '@mui/icons-material/ExpandMore'
 
 import type {IMenuItem} from '../model/IMenuItem'
 
 interface DashboardMenuItemProps {
   item: IMenuItem,
+  hasChildren?: boolean
   pl?: number,
+  open?: boolean,
+  onClick?: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 export default function DashboardMenuItem(props: DashboardMenuItemProps) {
-  const { item, pl = 2 } = props
+  const { item, hasChildren = false, pl = 2, open, onClick } = props
+
+  function clickHandler(event: React.MouseEvent<HTMLElement>): void {
+    event.preventDefault()
+
+    if (hasChildren && onClick) {
+      onClick(!open)
+    }
+  }
 
   return (
-      <ListItemButton href={item.route || ''} sx={{ pl }}>
-        <ListItemIcon>
-          <props.item.icon />
+      <ListItemButton href={item.route || ''} sx={{ pl }} onClick={clickHandler}>
+        <ListItemIcon sx={{ minWidth: '32px' }}>
+          <props.item.icon sx={{ fontSize: 'medium' }} />
         </ListItemIcon>
-        <ListItemText primary={item.name} />
+        <ListItemText primary={item.name} primaryTypographyProps={{ fontSize: '0.8rem' }} />
+        {hasChildren ? open ? <ExpandLess /> : <ExpandMore /> : null}
       </ListItemButton>
   )
 }
